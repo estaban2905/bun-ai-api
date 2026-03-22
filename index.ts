944d642388c7ac7a975189ba6685578ea2cde75f
max poblete <max@hostv6.entelchile.net>
2026-03-22 14:55:24 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Hello via Bun!");
+
+const server = Bun.serve({
+  port: process.env.PORT ?? 3000,
+  async fetch(req) {
+    return new Response("API de Bun está funcionando correctamente");
+  }
+})
+
+console.log(`Server is running on ${server.url}`)
